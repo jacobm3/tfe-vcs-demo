84b458194ee4215f0d49dcea20864de300f62d5c
Jacob <jacobm3@gmail.com>
2019-05-13 06:22:41 -0500

    Change bucket policy to private

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "jmartinson-demo-bucket"
-  acl    = "public"
+  acl    = "private"
 
   tags = {
     owner = "jmartinson@hashicorp.com"
